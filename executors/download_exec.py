90c03b88eb414d7997f2435ffd69414be02418ef
¨Corbanez97¨ <lucascorbanez@gmail.com>
2022-05-23 23:03:06 -0300

    Random Update

--- executors/download_exec.py
+++ executors/download_exec.py
@@ -5,10 +5,11 @@ class DownloaderExecutor:
     '''
     This class creates a downloading object and transports it to a landing folder.
     '''
-    def __init__(self, bucket_name: str, requests_arguments: dict, landing_directory: str, unzip: str = None) -> None:
+    def __init__(self, bucket_name: str, requests_arguments: dict, landing_directory: str, file_name: str = None, unzip: str = None) -> None:
         self.requests_arguments = requests_arguments ##A dictionary such as: {"url": URL, "params": PARAMS, ...}
         self.bucket_name = bucket_name
         self.landing_directory = landing_directory ##With file name in case of unzipped, without in case of zipped
+        self.file_name = file_name
         self.unzip = unzip
 
         self.res = requests.get(**self.requests_arguments)
@@ -16,7 +17,10 @@ class DownloaderExecutor:
         
         # self.py_exec_path = py_exec_path
         # self.py_exec_args = py_exec_args
-        
+
+    def get_client(self):
+        return boto3.client('s3')
+
     def download(self) -> None:
         
         if self.unzip == 'unzip':
@@ -29,10 +33,12 @@ class DownloaderExecutor:
                     Key = self.landing_directory + '/' + name,
                     Body = temp.open(name).read()
                 )
-            temp.extractall(self.landing_directory)
             
             
         elif not self.unzip:
             print('Will not unzip')
-            with open(self.landing_directory, 'wb') as temp:
-                temp.write(self.res.content)
+            s3.client.put_object(
+                Bucket = self.bucket_name,
+                Key = self.landing_directory + '/' + self.file_name,
+                Body = self.res.content
+            )
